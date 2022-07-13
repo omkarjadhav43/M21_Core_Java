cd8ca6a2aa1ca076a714a2e57bdf5f5051ea6eb6
hp <hp@LAPTOP-ORG7KQ7H>
2022-07-13 11:34:45 +0530

    first commit (second time)

---
+++ Day1_GitHub/src/com/capgemini/intro/program1.java
@@ -0,0 +1,12 @@
+package com.capgemini.intro;
+
+public class program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to c2tc");
+		
+
+	}
+
+}
