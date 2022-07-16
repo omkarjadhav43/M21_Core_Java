a9f0554bfaed103a34e056444e5c1cd357a3562b
hp <hp@LAPTOP-ORG7KQ7H>
2022-07-16 15:52:18 +0530

    First commit

---
+++ Day3_IntroToOOPS_ClassAndObjects/src/com/cg/default1/NonParameterizedConstructor.java
@@ -0,0 +1,18 @@
+package com.cg.default1;
+class A
+{
+	A()
+	{
+		System.out.println("Hey Omkar, I am using Constructor");
+	}
+}
+
+public class NonParameterizedConstructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A obj = new A();
+
+	}
+
+}
