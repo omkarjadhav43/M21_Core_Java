7b875a208a36a6ca8e9e0c38a17e90b868b003a5
hp <hp@LAPTOP-ORG7KQ7H>
2022-07-16 16:39:10 +0530

    First commit

---
+++ Day4_PackagesEncapsulationAbstraction/src/com/cg/pack1/Package1.java
@@ -0,0 +1,14 @@
+package com.cg.pack1;
+
+public class Package1 {
+	public String str = "Core Java";
+	public void print()
+	{
+		System.out.println("Welcome to "+str);
+	}
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
