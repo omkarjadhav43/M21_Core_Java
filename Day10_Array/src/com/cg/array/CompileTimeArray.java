8ac5be97f4a211912b94ce29f5fda1043fe7e315
hp <hp@LAPTOP-ORG7KQ7H>
2022-07-26 17:58:39 +0530

    First Commit

---
+++ Day10_Array/src/com/cg/array/CompileTimeArray.java
@@ -0,0 +1,12 @@
+package com.cg.array;
+
+public class CompileTimeArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {11,22,33,44,55};
+		System.out.println(arr[2]);
+
+	}
+
+}
