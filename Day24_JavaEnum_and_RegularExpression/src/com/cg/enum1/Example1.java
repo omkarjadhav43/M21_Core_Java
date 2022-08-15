57b93fd9e9838bdded49f3e9277071f11ee0abf4
hp <hp@LAPTOP-ORG7KQ7H>
2022-08-15 17:56:26 +0530

    First commit

---
+++ Day24_JavaEnum_and_RegularExpression/src/com/cg/enum1/Example1.java
@@ -0,0 +1,14 @@
+package com.cg.enum1;
+enum Car
+{
+	Mercedes,BMV,Audi;
+}
+public class Example1 {
+
+	public static void main(String[] args) {
+		Car c = Car.BMV;
+		System.out.println(c);
+	}
+}
+
+
